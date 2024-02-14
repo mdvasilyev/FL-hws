14a9824ad4495e93995a40a7d72c158666f88c3d
Maxim Vasilyev <mayxis@inbox.ru>
2024-02-15 00:49:16 +0300

    fix: style

--- project/task1.py
+++ project/task1.py
@@ -8,7 +8,9 @@ Info = namedtuple("Info", ["number_of_nodes", "number_of_edges", "edges"])
 
 def get_graph_info(name):
     graph = cfpq.graph_from_csv(cfpq.download(name))
-    return Info(graph.number_of_nodes(), graph.number_of_edges(), graph.edges(data=True))
+    return Info(
+        graph.number_of_nodes(), graph.number_of_edges(), graph.edges(data=True)
+    )
 
 
 def two_cycle_graph(path, n, m, labels=("a", "b")):
