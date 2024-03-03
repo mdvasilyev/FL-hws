bec8f3e03096099a0019b4f1b773fb4959ed550a
Maxim Vasilyev <mayxis@inbox.ru>
2024-03-03 14:30:09 +0300

    fix: add edge cases to graph_to_nfa func

--- project/task2.py
+++ project/task2.py
@@ -1,19 +1,27 @@
+from networkx import MultiDiGraph
+from typing import Set
 from pyformlang.regular_expression import Regex
-from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
+from pyformlang.finite_automaton import NondeterministicFiniteAutomaton, State
 
 
 def regex_to_dfa(reg):
     regex = Regex(reg)
     nfa = regex.to_epsilon_nfa()
-    return nfa.minimize()
+    return nfa.to_deterministic().minimize()
 
 
-def graph_to_nfa(graph, start, final):
+def graph_to_nfa(graph: MultiDiGraph, start: Set[int], final: Set[int]):
     nfa = NondeterministicFiniteAutomaton(graph)
+    if not start:
+        for i in graph.nodes():
+            start.add(i)
+    if not final:
+        for i in graph.nodes():
+            final.add(i)
     for node in start:
-        nfa.add_start_state(node)
+        nfa.add_start_state(State(node))
     for node in final:
-        nfa.add_final_state(node)
+        nfa.add_final_state(State(node))
     for v, u, data in graph.edges(data=True):
         nfa.add_transition(v, data["label"], u)
     return nfa
