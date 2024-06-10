9ee1b9e92f376e11fc9070509d2a0660c429eefe
Maxim Vasilyev <mayxis@inbox.ru>
2024-06-10 12:03:24 +0300

    fix: back to test all tasks

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -5,9 +5,8 @@ import shared
 def main():
     shared.configure_python_path()
     subprocess.check_call(
-        ["python", "-m", "pytest", "-vv", "-s", "./tests/autotests/test_task11.py"]
+        ["python", "-m", "pytest", "-vv", "-s", shared.TESTS]
     )
-    # shared.TESTS
 
 
 if __name__ == "__main__":
